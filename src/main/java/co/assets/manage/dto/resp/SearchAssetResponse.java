35cb06269e034fa551cdaf514b0eb70236849c29
zjq <jqchu96@outlook.com>
2026-02-27 19:09:36 +0900

    update readme

--- src/main/java/co/assets/manage/dto/resp/SearchAssetResponse.java
+++ src/main/java/co/assets/manage/dto/resp/SearchAssetResponse.java
@@ -3,10 +3,12 @@ package co.assets.manage.dto.resp;
 /**
  * Assetを検索するレスポンスクラス（不要なフィールドを除外）
  *
+ * @param id assetId
  * @param title    Asset title
  * @param filePath Asset filePath
  */
 public record SearchAssetResponse(
+        String id,
         String title,
         String filePath
 ) {
